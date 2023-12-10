cc24f77a1f5728268de8dcdd86dd339158608486
artem2284708 <145186264+artem2284708@users.noreply.github.com>
2023-12-11 02:17:33 +0300

    Update Jupiter.py

--- Jupiter.py
+++ Jupiter.py
@@ -249,7 +249,7 @@ if liga == 'RU1':
   ab = offsetbox.AnnotationBbox(imagebox, (16, 100.500), frameon=False)
   ax.add_artist(ab)
 
-st.scatter_chart(plt)
+st.scatter_chart(fig)
 
 """---
 Open new file to merge cool data
